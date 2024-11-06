cc2566eff31d5b5bae88c6801eed1590447c41d6
IGNACIO ANDRADE <ignacio.andrade1221@gmail.com>
2024-11-06 17:49:33 -0300

    ticket2 avance

--- data.js
+++
@@ -1,9 +0,0 @@
-<script>
-    // Seleccionamos el textarea y agregamos un evento para ajustar la altura automáticamente
-    const textarea = document.querySelector('textarea');
-
-    textarea.addEventListener('input', function() {
-        this.style.height = 'auto'; // Resetea la altura
-        this.style.height = (this.scrollHeight) + 'px'; // Ajusta la altura al contenido
-    });
-</script>
